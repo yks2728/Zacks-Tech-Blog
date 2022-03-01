5104354f907f790468b7c7d2676cd565178fe1f4
Zack Greenfield <zackg007@verizon.net>
2022-03-01 10:54:46 -0500

    installed bcrypt and created hooks

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {}
 
@@ -36,8 +37,16 @@ User.init(
         }
     },
     {
-        // TABLE CONFIGURATION OPTIONS GO HERE
-
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
